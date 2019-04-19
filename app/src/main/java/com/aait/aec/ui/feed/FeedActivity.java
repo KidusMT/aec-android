ff8e25b5b051fd3a6b149345f6e59db2b9e1ef76
Kidus M. Tekeste <kidusmamuye@gmail.com>
2019-04-19 23:07:51 +0300

    finished the category UI

--- app/src/main/java/com/aait/aec/ui/feed/FeedActivity.java
+++ app/src/main/java/com/aait/aec/ui/feed/FeedActivity.java
@@ -20,10 +20,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 
 import com.aait.aec.R;
